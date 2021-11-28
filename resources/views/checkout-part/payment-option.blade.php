abab3ded8e13cc7b408ac85b1180778da15b668f
eLDoherty <leonardodoherty@gmail.com>
2021-11-28 12:30:54 +0700

    up

--- resources/views/checkout-part/payment-option.blade.php
+++ resources/views/checkout-part/payment-option.blade.php
@@ -1,5 +1,22 @@
 <section class="triplay-payment">
     <div class="container">
+        {{--  --}}
+        <div class="manual-payment">
+            <h3>Pembayaran <span>Manual</span></h3>
+            <a href="#triplay-manual-payment">
+                <div class="card-payment manual-direct">
+                    <img src="{{ asset('img/allpayment.png') }}">
+                    <div class="total-price">
+                        <p>Harga :</p>
+                        <span>10000</span>
+                    </div>
+                    <div class="inc-ppn">
+                        <p>Lebih Murah & Sudah Termasuk PPN</p>
+                    </div>
+                </div>
+            </a>
+        </div>
+        {{--  --}}
         <h3>Pembayaran <span>Otomotis</span></h3>
         <div class="auto-payment">
             <div class="card-payment bca-payment">
@@ -22,21 +39,6 @@
                     <p>Sudah termasuk PPN</p>
                 </div>
             </div>
-        </div>
-        <div class="manual-payment">
-            <h3>Pembayaran <span>Manual</span></h3>
-            <a href="#triplay-manual-payment">
-                <div class="card-payment manual-direct">
-                    <img src="{{ asset('img/allpayment.png') }}">
-                    <div class="total-price">
-                        <p>Harga :</p>
-                        <span>10000</span>
-                    </div>
-                    <div class="inc-ppn">
-                        <p>Lebih Murah & Sudah Termasuk PPN</p>
-                    </div>
-                </div>
-            </a>
         </div>
     </div>
 </section>
