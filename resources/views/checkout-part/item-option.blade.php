eab12107fe8994d8c73ca35d1764d03dfc2d1566
eLDoherty <leonardodoherty@gmail.com>
2021-12-01 15:57:08 +0700

    logic

--- resources/views/checkout-part/item-option.blade.php
+++ resources/views/checkout-part/item-option.blade.php
@@ -3,7 +3,11 @@
         <h3>Pilih <span>Diamond</span></h3>
         <div class="button-option__wrapper">
             @foreach ($dataPrices as $data)
-            <button class="option-item" onclick="getValueItemButton(this);" value="{{ $data->price }}">{{ $data->item }}</button>   
+            @if ( is_nan($data->price) || $data->price == null )
+                <button class="option-item" onclick="getValueItemButton(this);" value="{{ $data->harga }}">{{ $data->item }}</button>   
+            @else 
+            <button class="option-item" onclick="getValueItemButton(this);" value="{{ $data->price }}">{{ $data->item }}</button>  
+            @endif 
             @endforeach
         </div>
     </div>
